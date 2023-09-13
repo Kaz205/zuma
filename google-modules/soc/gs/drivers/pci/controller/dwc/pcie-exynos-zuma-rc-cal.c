ebf32f6958656a74f52b6e0d4700ba6846beb956
Lokesh Sharma <sharma.l@samsung.com>
2023-09-12 21:49:20 -0700

    drivers: pcie: Enable slv pending status check
    As per S2D dump during resume PCIe had read request pending so enabled
    slave transfer pending bit during link up which won't allow L1SS entry
    if any transaction is pending
    
    Bug: 299070142
    Change-Id: Ib83ebd6aa5c036d044f801528e4f6738ac8db89a
    Signed-off-by: Lokesh sharma <sharma.l@samsung.com>

--- google-modules/soc/gs/drivers/pci/controller/dwc/pcie-exynos-zuma-rc-cal.c
+++ google-modules/soc/gs/drivers/pci/controller/dwc/pcie-exynos-zuma-rc-cal.c
@@ -347,6 +347,9 @@ void exynos_pcie_rc_pcie_phy_config(struct exynos_pcie *exynos_pcie, int ch_num)
 		writel(0x0, elbi_base_regs + 0x1408);
 		writel(0x0, elbi_base_regs + 0x1400);
 
+		/* slv pend check */
+		writel(0x1, elbi_base_regs + PCIE_SLV_PEND_SEL_NAK);
+
 		/* pma_setting */
 
 		//Common
@@ -569,6 +572,9 @@ void exynos_pcie_rc_pcie_phy_config(struct exynos_pcie *exynos_pcie, int ch_num)
 		writel(0x0, elbi_base_regs + 0x1404);
 		writel(0x0, elbi_base_regs + 0x1408);
 		writel(0x0, elbi_base_regs + 0x1400);
+
+		/* slv pend check */
+		writel(0x1, elbi_base_regs + PCIE_SLV_PEND_SEL_NAK);
 
 		/* pma_setting */
 
