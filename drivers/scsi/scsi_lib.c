2bc11890adc8b8d5d7fe2f0d633b91d30437f66e
Bart Van Assche <bvanassche@acm.org>
2024-04-10 22:24:22 -0400

    Reapply d2746cd ("scsi: core: Rename scsi_mq_done() into scsi_done() and export it")
    [ Upstream commit a710eacb9d13cb5d9eb5341ebc6fc8f7b96f8c6f ]
    
    Since the removal of the legacy block layer there is only one completion
    function left in the SCSI core, namely scsi_mq_done(). Rename it into
    scsi_done(). Export that function to allow SCSI LLDs to call it directly.
    
    Link: https://lore.kernel.org/r/20211007202923.2174984-3-bvanassche@acm.org
    Reviewed-by: Benjamin Block <bblock@linux.ibm.com>
    Reviewed-by: Bean Huo <beanhuo@micron.com>
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Stable-dep-of: e193b7955dfa ("RDMA/srp: Do not call scsi_done() from srp_abort()")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    [@0ctobot: This corrects implicit declaration of 'scsi_done' subsequent to
    172800c ("scsi: usb: Call scsi_done() directly")]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- drivers/scsi/scsi_lib.c
+++ drivers/scsi/scsi_lib.c
@@ -1576,7 +1576,7 @@ static blk_status_t scsi_prepare_cmd(struct request *req)
 	return scsi_cmd_to_driver(cmd)->init_command(cmd);
 }
 
-static void scsi_mq_done(struct scsi_cmnd *cmd)
+void scsi_done(struct scsi_cmnd *cmd)
 {
 	if (unlikely(blk_should_fake_timeout(scsi_cmd_to_rq(cmd)->q)))
 		return;
@@ -1585,6 +1585,7 @@ static void scsi_mq_done(struct scsi_cmnd *cmd)
 	trace_scsi_dispatch_cmd_done(cmd);
 	blk_mq_complete_request(scsi_cmd_to_rq(cmd));
 }
+EXPORT_SYMBOL(scsi_done);
 
 static void scsi_mq_put_budget(struct request_queue *q, int budget_token)
 {
@@ -1689,7 +1690,7 @@ static blk_status_t scsi_queue_rq(struct blk_mq_hw_ctx *hctx,
 
 	scsi_set_resid(cmd, 0);
 	memset(cmd->sense_buffer, 0, SCSI_SENSE_BUFFERSIZE);
-	cmd->scsi_done = scsi_mq_done;
+	cmd->scsi_done = scsi_done;
 
 	blk_mq_start_request(req);
 	reason = scsi_dispatch_cmd(cmd);
