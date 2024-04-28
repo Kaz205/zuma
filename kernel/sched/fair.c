705c2186bb32f06c63002fc3272e4a85ba4d9930
Kazuki H <kazukih0205@gmail.com>
2024-04-29 03:00:33 +0900

    Revert "sched/eevdf: Fixup PELT vs DELAYED_DEQUEUE"
    This reverts commit 99fecf61c3f0681727b57e2edabe505d27375ac0.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5013,7 +5013,6 @@ dequeue_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
 			if (cfs_rq->next == se)
 				cfs_rq->next = NULL;
 			se->sched_delayed = 1;
-			update_load_avg(cfs_rq, se, 0);
 			return false;
 		}
 	}
@@ -6192,7 +6191,6 @@ requeue_delayed_entity(struct sched_entity *se)
 	}
 
 	se->sched_delayed = 0;
-	update_load_avg(cfs_rq, se, 0);
 }
 
 /*
